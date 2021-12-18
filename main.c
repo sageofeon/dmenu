a747333581c26e5a0de03449d6964053a9d87f10
sergebash <sergebash@gmail.com>
2021-12-19 00:02:20 +0300

    Add linux version

--- main.c
+++ main.c
@@ -8,10 +8,17 @@
 *	For exit press ESC key
 ******************************************************/
 
+#ifdef __linux__ 
 #include <stdio.h>
-#include <string.h>
 #include <stdlib.h>
 #include <conio.h>
+#elif _WIN32
+#include "curses.h"
+#include "wchar.h"
+#include "locale.h"
+#else
+#endif
+#include "string.h"
 #define	PRINT( val ) printf("\n %s\n", val);
 
 /*** DRAWS A BORDER AROUND THE ITEM ***/
@@ -65,8 +72,26 @@ unsigned int  menu_items(  char *item[],
 void menu( char *item[],  unsigned int count_item )
 {
     unsigned int crtl, ch = 0,
-                    cur_item = 0;
-
+                 cur_item = 0;
+#ifdef __linux__
+   menu_items( item, cur_item, count_item, 0);
+   while ( 1 )
+    {
+        crtl = getch();
+        if ( crtl == 27 )
+        {
+            crtl = getch();
+            if ( crtl == 91 )
+            {
+                ch = getch();
+                clear(); 
+                refresh();
+                cur_item = menu_items( item, cur_item, count_item, ch );
+                PRINT("");
+            }
+        }	
+    } 
+#elif _WIN32
     system("cls");
     menu_items( item, cur_item, count_item, 0);
 
@@ -80,13 +105,31 @@ void menu( char *item[],  unsigned int count_item )
         system( "cls" );
         cur_item = menu_items( item, cur_item, count_item, ch );
     }
+#else
+#endif
 }
+
 
 int main()
 {
+#ifdef __linux__
+    setlocale(LC_ALL, "ru_RU.UTF-8");
+
+    WINDOW *wnd;
+
+    wnd = initscr(); // curses call to initialize window
+    cbreak(); // curses call to set no waiting for Enter key
+    noecho(); // curses call to set no echoing
+    clear(); // curses call to clear screen, send cursor to position (0,0)
+    refresh(); // curses call to implement all changes since last refresh
+#endif
+    
     char 	*item[] = { "item1", "item2",  "item3", "item4", "item5", "item6", "item7" };
     unsigned int count = sizeof(item) / sizeof(item[0]);
 
     menu( item,  count );
+#ifdef __linux__
+    endwin(); // curses call to restore the original window and leave
+#endif
     return 0;
 }
